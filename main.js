0cbbb0b863665f95dfc79b36f37dd4441e6f5856
lilia <liliakai@gmail.com>
2017-03-14 19:22:48 -0700

    Set AUMID to appId
    It is also important to set the Application User Model ID (AUMID) to the
    appId of the application, in order for notifications on Windows 8/8.1 to
    function and for Window 10 notifications to display the app icon within
    the notifications by default. The AUIMD should be set within the Main
    process and before any BrowserWindows have been opened, it is normally
    the first piece of code executed.
    
    https://github.com/electron-userland/electron-builder/wiki/NSIS#guid-vs-application-name

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+app.setAppUserModelId("org.whispersystems.electron-quick-start")
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
